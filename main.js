f97ec8b6bcdb2d994c946318250f8d85b5de334b
ju-marin14 <julia.marin.silveira@escola.pr.gov.br>
2026-06-16 11:50:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,10 @@ for(let i=0; i <botoes.length; i++){
 
         botoes[i].classlit.add("ativo");
     }
-}
+} const contadores = document.querySelectorAll(".contador");
+  const tempoObjetivo1 = new Date("2024-10-05t00:00:00");
+  const tempoObjetivo2 = new Date("2024-12-05t00:00:00");
+  const tempoObjetivo3 = new Date("2024-12-30t00:00:00");
+  const tempoObjetivo4 = new Date("2025-02-01t00:00:00");
+
+  const tempos = [tempo tempoObjetivo1,tempoObjetivo2,tempo
