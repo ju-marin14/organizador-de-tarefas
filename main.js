78b4acb2b482afbb5bf9b1019a53052593770cc6
ju-marin14 <julia.marin.silveira@escola.pr.gov.br>
2026-06-30 11:49:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,18 @@ for(let i=0; i <botoes.length; i++){
   function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo-tempoAtual;
-    let egundo = Math.floor(tempoFinal/1000);
-    let 
+    let segundo = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos%= 60;
+    minutos%= 60;
+    horas%= 24;
+    if (tempoFinal > 0){
+        return [0,0,0,0];
+    }
   }
+
+  function atualizaCronometro(){
+    for (let i=0; i<contadores.l)
